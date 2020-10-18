a1014b89cdfccef92331265063bc242c809c6ac9
jinyung2 <jinyoungchoi118@gmail.com>
2020-10-18 15:12:15 -0700

    created src folder, and added test folder and testing frameworks

---
+++ src/validAnagram.ts
@@ -0,0 +1,7 @@
+function validAnagram(str1: String, str2: String) {
+
+}
+
+validAnagram('cat', 'cat');
+validAnagram('', 'cat');
+validAnagram('cat', 'cat');
---
+++ test/sameFrquency1.spec.ts
@@ -0,0 +1,5 @@
+import {expect, assert, should} from 'chai';
+
+it('should pass', () => {
+    expect(true).to.equal(true);
+})
