119300df5f72236cf621b3401ab61f175509344c
jinyung2 <jinyoungchoi118@gmail.com>
2020-10-18 17:42:50 -0700

    added unit testing via mocha/chai for anagram exercise

--- src/validAnagram.ts
+++ src/validAnagram.ts
@@ -1,7 +1,33 @@
-function validAnagram(str1: String, str2: String) {
+// function should check if the two strings are valid anagrams of each other
+// must contain same # of characters from each string
+// constraints: str1 and str2 are valid strings with only letters
 
-}
+export function validAnagram(str1: String, str2: String): boolean {
+    // trivial case, length is different
+    if (str1.length !== str2.length) {
+        return false;
+    }
 
-validAnagram('cat', 'cat');
-validAnagram('', 'cat');
-validAnagram('cat', 'cat');
+    // convert both strings into an object with lower case letters
+    let obj1: {[key: string]: number} = {};
+    let obj2: {[key: string]: number} = {};
+    for (let c of str1) {
+        obj1[c] = obj1.hasOwnProperty(c) ? ++obj1[c] : 1;
+    }
+    for (let c of str2) {
+        obj2[c] = obj2.hasOwnProperty(c) ? ++obj2[c] : 1;
+    }
+    for (let c of str1) {
+        // check that the map contains it
+        if (obj1.hasOwnProperty(c) && obj2.hasOwnProperty(c)) {
+            if (obj1[c] !== obj2[c]) {
+                return false;
+            }
+        } else {
+            // if it doesnt enter conditional, then they both have c
+            return false;
+        }
+        
+    }
+    return true;
+}   
--- test/sameFrquency1.spec.ts
+++ test/sameFrquency1.spec.ts
@@ -1,5 +1,19 @@
-import {expect, assert, should} from 'chai';
+import {expect, assert, should } from 'chai';
+import { validAnagram } from '../src/validAnagram';
 
-it('should pass', () => {
-    expect(true).to.equal(true);
+describe('tests for validAnagram.ts', () => {
+    it('should return true when input is anagram', () => {
+        // using expect syntax
+        expect(validAnagram('tace', 'eatc')).to.be.true;
+    }),
+    it ('should return false when inputs not anagram', () => {
+        // using assert
+        assert.equal(validAnagram('catt', 'caat'), false);
+    }),
+    it('should pass with mixed case (lower and upper)', () => {
+        // using should syntax
+        should();
+        validAnagram.should.be.a('function');
+        validAnagram('cat', "CAT").should.be.false;
+    })
 })
