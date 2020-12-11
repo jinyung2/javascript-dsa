c9a45700906686fcb559affbc0494c2f922ad15f
jinyung2 <jinyoungchoi118@gmail.com>
2020-12-10 22:36:16 -0800

    completed recursive string reverse

---
+++ test/recursion/reverse.spec.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { reverse } from '../../src/recursion/reverse';
+
+describe('String Reverse', () => {
+    it('should reverse a string', () => {
+        expect(reverse('awesome')).to.equal('emosewa');
+        expect(reverse('awesomee')).to.equal('eemosewa');
+    }),
+    it('should return an empty string', () => {
+        expect(reverse('')).to.equal('');
+    })
+})
