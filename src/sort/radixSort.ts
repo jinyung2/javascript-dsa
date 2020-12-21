6249e29fbb8c2d08601bf67f22a8887ccae67b4f
jinyung2 <jinyoungchoi118@gmail.com>
2020-12-20 16:24:23 -0800

    radix sort! cool stuff

--- src/sort/radixSort.ts
+++ src/sort/radixSort.ts
@@ -1,13 +1,91 @@
-import { expect } from 'chai';
-import { radixSort } from '../../src/sort/radixSort';
 
-describe('Radix Sort', () => {
-    it('should sort numerical arrays correctly', () => {
-        expect(radixSort([3,2,5,6,1,2])).to.eql([1,2,2,3,5,6]);
-        expect(radixSort([-1,0,-2,-4,5,2])).to.eql([-4,-2,-1,0,2,5]);
-        expect(radixSort([0,0,0,0,0])).to.eql([0,0,0,0,0]);
-    });
-    it('should words in lexicographical order', () => {
-        expect(radixSort(['hi', 'HI', 'hello', 'whatsup', 'ayyylmao'])).to.eql(["HI", "ayyylmao", "hello", "hi", "whatsup"]);
-    })
-})
+/**
+ * 
+ * @param arr array being sorted via radix sort (non comparison based)
+ */
+export function radixSort(arr: any[]): any[] {
+    let maxDigitCount = mostDigits(arr);
+    for (let i = 0; i < maxDigitCount; i++) {
+        let buckets: any[] = Array.from({length: 10}, () => []);
+        for (let e of arr) {
+            buckets[getDigitMath(e, i)].push(e);
+        }
+        arr = [].concat(...buckets);
+    }
+    return arr;
+}
+
+/**
+ * returns the number of digits of the biggest number in array
+ * @param arr array with numbers
+ * @param digitCount helper function that counts number of digits
+ */
+export function mostDigits(arr: number[], dCount: Function = digitCount): number {
+    let maxNumDigits = 0;
+    for (let num of arr) {
+        maxNumDigits = Math.max(maxNumDigits, digitCount(num));
+    }
+    return maxNumDigits;
+}
+
+/**
+ * Outputs the # of digits in a number
+ * @param num number being inputted
+ */
+export function digitCount(num: number) {
+    if (num === 0) {
+        return 1;
+    }
+    return Math.floor(Math.log10(Math.abs(num)) + 1);
+}
+
+/**
+ * returns the digit at the specified place value (right to left starting from index 0)
+ * @param num inputted number
+ * @param place place we are searching in number
+ * @param base the base being divided (decimal, octal, binary, etc...)
+ */
+export function getDigit(num: number, place: number, base: number = 10): number {
+    // if place exceeds highest place, return 0
+    if (place > num.toString().length) {
+        return 0;
+    } 
+    let counter = 0;
+    while (num > 0 && counter <= place) {
+        let digit = num % base;
+        if (counter === place) {
+            return digit;
+        }
+        num = Math.floor(num/base);
+        counter++;
+    }
+    return 0;
+}
+
+/**
+ * returns the digit at specified place value recursively (right to left starting index 0)
+ * @param num inputted number
+ * @param place place we are searching in number
+ * @param base the based being divided(decimal, octal, binary, etc...)
+ */
+export function getDigitRecursive(num: number, place: number, base: number = 10): number {
+    if (num <= 0) {
+        return 0;
+    }
+    let digit = num % base;
+    if (place === 0) {
+        return digit;
+    }
+    return getDigitRecursive(Math.floor(num/base), place - 1, base);
+}
+
+/**
+ * returns the digit at specified place value mathematically formulated (right to left starting index 0)
+ * constant time due to mathematical computation
+ * @param num inputted number
+ * @param place place we are searching in number
+ * @param base the based being divided(decimal, octal, binary, etc...)
+ */
+export function getDigitMath(num: number, place: number, base: number = 10): number {
+    return Math.floor(Math.abs(num) / Math.pow(base, place) % base);
+}
--- test/sort/radixSort.spec.ts
+++ test/sort/radixSort.spec.ts
@@ -1,3 +1,41 @@
-export function radixSort(arr: any[]): any[] {
-    
-}
+import { expect } from 'chai';
+import { radixSort, getDigit, getDigitRecursive, getDigitMath, digitCount, mostDigits} from '../../src/sort/radixSort';
+
+describe('Radix Sort', () => {
+    it('should return correct digit count', () => {
+        expect(digitCount(123)).to.equal(3);
+        expect(digitCount(0)).to.equal(1);
+        expect(digitCount(123523)).to.equal(6);
+    });
+    it('should return most digits of a list of numbers', () => {
+        expect(mostDigits([1,2,3,4444444], digitCount)).to.equal(7);
+        expect(mostDigits([1,2,3,444], digitCount)).to.equal(3);
+        expect(mostDigits([111111111,1,2,3,4444444], digitCount)).to.equal(9);
+    })
+    it('should return correct digit', () => {
+        expect(getDigit(1234, 0, 10)).to.equal(4);
+        expect(getDigit(1234, 1, 10)).to.equal(3);
+        expect(getDigit(1234, 2, 10)).to.equal(2);
+        expect(getDigit(1234, 3, 10)).to.equal(1);
+        expect(getDigit(1234, 4, 10)).to.equal(0);
+    });
+    it('should return correct digit (recursive)', () => {
+        expect(getDigitRecursive(1234, 0, 10)).to.equal(4);
+        expect(getDigitRecursive(1234, 1, 10)).to.equal(3);
+        expect(getDigitRecursive(1234, 2, 10)).to.equal(2);
+        expect(getDigitRecursive(1234, 3, 10)).to.equal(1);
+        expect(getDigitRecursive(1234, 4, 10)).to.equal(0);
+    });
+    it('should return correct digit (Math)', () => {
+        expect(getDigitMath(1234, 0, 10)).to.equal(4);
+        expect(getDigitMath(1234, 1, 10)).to.equal(3);
+        expect(getDigitMath(1234, 2, 10)).to.equal(2);
+        expect(getDigitMath(1234, 3, 10)).to.equal(1);
+        expect(getDigitMath(1234, 4, 10)).to.equal(0);
+    });
+    it('should sort numerical arrays correctly', () => {
+        expect(radixSort([3,2,5,6,1,2])).to.eql([1,2,2,3,5,6]);
+        expect(radixSort([0,0,0,0,0])).to.eql([0,0,0,0,0]);
+        expect(radixSort([1,2343,4454,221133,533,5532,13443,2222334])).to.eql([1, 533, 2343, 4454, 5532, 13443, 221133, 2222334]);
+    });
+})
